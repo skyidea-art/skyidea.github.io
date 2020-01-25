66e16d940e893a55afa4b21fe227f02470733609
DeveloperMode0 <41708091+DeveloperMode0@users.noreply.github.com>
2020-01-25 22:24:03 +1300

    _edited25012020

---
+++ _require/javascript.js
@@ -0,0 +1,10 @@
+        function active()
+        {
+            document.getElementById('menu').style.width = '250px';
+            document.getElementById('content').style.marginLeft = '250px';
+        }
+        function deactivate()
+        {
+            document.getElementById('menu').style.width = '0px';
+            document.getElementById('content').style.marginLeft = '0px';
+        }
